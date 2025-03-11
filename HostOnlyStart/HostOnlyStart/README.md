83516a668f2f85e576e9edf9138f616d56aee708
linkoid <36754150+linkoid@users.noreply.github.com>
2025-03-11 15:57:16 -0400

    Add project files.

---
+++ HostOnlyStart/HostOnlyStart/README.md
@@ -0,0 +1,10 @@
+# Host Only Start
+Only the host can send a message to the taxman to leave the shop or enter a level.
+All players will still be able to send a message to the taxman to leave a level.
+
+Icon by @s1ckboy on Discord.
+
+## Bugs / Contact
+The fastest way to contact me is via Discord.
+You can find me on the R.E.P.O. Modding Discord [HostOnlyStart Thread](https://discord.com/channels/1344557689979670578/1349104796917043221),
+or in my [Deja Drift Discord Server](https://discord.gg/yKwt2AWcGF).
---
+++ HostOnlyStart/HostOnlyStart/TruckScreenTextPatches.cs
@@ -0,0 +1,81 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace Linkoid.Repo.HostOnlyStart;
+
+[HarmonyPatch(typeof(TruckScreenText))]
+internal static class TruckScreenTextPatches
+{
+    [HarmonyPrefix, HarmonyPatch(nameof(TruckScreenText.HoldChat))]
+    private static void HoldChat_Prefix(TruckScreenText __instance)
+    {
+        // Code to execute for each PlayerController *before* Update() is called.
+        HostOnlyStart.Logger.LogDebug($"{__instance} Update Prefix");
+    }
+
+    [HarmonyTranspiler, HarmonyPatch(nameof(TruckScreenText.HoldChat))]
+    private static IEnumerable<CodeInstruction> HoldChat_Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var smeth_ProtonNetwork_get_IsMasterClient = AccessTools.PropertyGetter(typeof(PhotonNetwork), nameof(PhotonNetwork.IsMasterClient));
+        var smeth_HostOnlyStartCheck = AccessTools.DeclaredMethod(typeof(TruckScreenTextPatches), nameof(HostOnlyStartCheck));
+
+        var head = new CodeMatcher(instructions);
+
+        // Match:
+        //     if (!PhotonNetwork.IsMasterClient) return;
+        CodeMatch[] notMasterClient_return =
+        {
+            new(OpCodes.Call, smeth_ProtonNetwork_get_IsMasterClient),
+            new(x => x.opcode == OpCodes.Brfalse),
+        };
+        head.MatchForward(useEnd: true, notMasterClient_return);
+        head.ThrowIfInvalid($"Could not match {nameof(notMasterClient_return)}");
+
+        // Get return label
+        HostOnlyStart.Logger.LogDebug($"head.Instruction.opcode: {head.Instruction.opcode.Value}");
+        HostOnlyStart.Logger.LogDebug($"head.Instruction.operand: {head.Instruction.operand}");
+        var returnLabel = head.Instruction.operand;
+
+        head.Advance(1);
+
+        // Insert:
+        //     if (!HostOnlyStartCheck(this)) return;
+        head.Insert(
+            new CodeInstruction(OpCodes.Ldarg_0),
+            new CodeInstruction(OpCodes.Call, smeth_HostOnlyStartCheck),
+            new CodeInstruction(OpCodes.Brfalse, returnLabel)
+        );
+
+        return head.InstructionEnumeration();
+    }
+
+    private static bool HostOnlyStartCheck(TruckScreenText __instance)
+    {
+        HostOnlyStart.Logger.LogDebug($"HostOnlyStartCheck");
+        if (RunManager.instance == null)
+            return false;
+
+        Level[] hostOnlyStartLevels =
+        {
+            RunManager.instance.levelArena,
+            RunManager.instance.levelLobby,
+            RunManager.instance.levelLobbyMenu,
+            RunManager.instance.levelRecording,
+            RunManager.instance.levelShop,
+            RunManager.instance.levelMainMenu,
+        };
+
+        HostOnlyStart.Logger.LogDebug($"hostOnlyStartLevels.Contains(RunManager.instance.levelCurrent) = {hostOnlyStartLevels.Contains(RunManager.instance.levelCurrent)}");
+        HostOnlyStart.Logger.LogDebug($"!__instance.staticGrabObject.playerGrabbing.Contains(PlayerAvatar.instance.physGrabber) = {!__instance.staticGrabObject.playerGrabbing.Contains(PlayerAvatar.instance.physGrabber)}");
+
+        if (hostOnlyStartLevels.Contains(RunManager.instance.levelCurrent)
+            && !__instance.staticGrabObject.playerGrabbing.Contains(PlayerAvatar.instance.physGrabber))
+            return false;
+
+        return true;
+    }
+}
